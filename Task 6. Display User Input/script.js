fce22fea63783d1e6820f99d1b896a792c649e4f
Ani-Eremyan <114775118+Ani-Eremyan@users.noreply.github.com>
2024-02-23 05:02:00 +0400

    Add files via upload

---
+++ Task 6. Display User Input/script.js
@@ -0,0 +1,5 @@
+c/*onst results = document.getElementById("user_input");*/
+function Display() {
+    let text = document.getElementById("user_input").value;
+    document.getElementById("show").textContent = text;
+}
